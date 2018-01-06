727e3f12683296b900d6a092cb6782e07eac7727
cdpolymath <cdpolymath@gmail.com>
2018-01-06 23:02:01 +0100

    implementation of the converter

--- str2txt.cpp
+++ str2txt.cpp
@@ -13,22 +13,45 @@ void help()
 int main(int argc, char *argv[])
 {
 
+  // Check the command line
+
   if (argc < 1 or strcmp(argv[1], "-help") == 0)
   {
     help();
     return EXIT_FAILURE;
   }
 
-  std::ifstream file;
-  file.open(argv[1]);
+  // Try to open the subtitle file (.srt) in reading mode
 
-  if (not file.is_open())
+  std::string srt_filename = argv[1];
+  std::ifstream srt_file;
+  srt_file.open(srt_filename.c_str());
+
+  if (not srt_file.is_open())
   {
-    std::cerr << "Unable to open file " << argv[1] << std::endl;
+    std::cerr << "Unable to open srt file " << srt_filename << std::endl;
     return EXIT_FAILURE;
   }
 
-  file.close();
+  // Try to open the text file (.txt) in writing mode 
+  
+  std::size_t ext_index = srt_filename.find_last_of("."); 
+  std::string txt_filename = srt_filename.substr(0, ext_index) + ".txt"; 
+  std::ofstream txt_file(txt_filename.c_str());
+
+  // Convert the srt file into a txt one
+
+  std::string line;
+  while (std::getline(srt_file, line))
+  {
+    std::getline(srt_file, line);
+    std::getline(srt_file, line);
+    txt_file << line << " ";
+    std::getline(srt_file, line);
+  } 
+
+  txt_file.close();
+  srt_file.close();
   
 
   
